431fb92b82e205f9f736106db429db4633bf2f3c
NarithNR9 <79370506+NarithNR9@users.noreply.github.com>
2023-04-04 15:03:40 +0700

    owner bookings

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -1,63 +1,56 @@
-const db = require('../config/database')
+const db = require("../config/database");
 
 exports.getAllBookings = (req, res, next) => {
-  const today = new Date().toISOString().slice(0, 10)
+  const today = new Date().toISOString().slice(0, 10);
   db.execute(`SELECT * FROM booking WHERE date >= '${today}'`)
     .then((result) => {
       result[0].map((ele) => {
-        ele.date = ele.date.toISOString().slice(0, 10)
-      })
-      res.status(200).json(result[0])
+        ele.date = ele.date.toISOString().slice(0, 10);
+      });
+      res.status(200).json(result[0]);
     })
-    .catch((err) => console.log(err))
-}
+    .catch((err) => console.log(err));
+};
 
 exports.getMyBookings = (req, res, next) => {
-  const pId = req.params.playerId
-  const today = new Date().toISOString().slice(0, 10)
-  db.execute(`SELECT b.booking_id,b.field_id, f.fieldName, b.date, b.time, b.pitch_number, b.status  FROM SportField.booking b join SportField.field f on b.field_id = f.field_id where b.player_id = '${pId}' order by b.date DESC;`)
+  const pId = req.params.playerId;
+  db.execute(
+    `SELECT b.booking_id,b.field_id, f.fieldName, b.date, b.time, b.pitch_number, b.status  FROM SportField.booking b join SportField.field f on b.field_id = f.field_id where b.player_id = '${pId}' order by b.date DESC;`
+  )
     .then((result) => {
-      res.status(200).json(result[0])
+      res.status(200).json(result[0]);
     })
-    .catch((err) => console.log(err))
-}
+    .catch((err) => console.log(err));
+};
+
+// get owner booking
+exports.getOwnerBooking = (req, res, next) => {
+  const { fieldId, date, type, pitchNumber } = req.query;
+  db.execute(
+    `SELECT b.booking_id, f.fieldName,f.type, b.date, b.time, b.pitch_number, b.status, p.phone_number  FROM SportField.booking b join SportField.field f on b.field_id = f.field_id join SportField.player p on b.player_id = p.player_id where f.field_id = '${fieldId}' AND f.type = '${type}' AND b.pitch_number = '${pitchNumber}' AND b.date LIKE '${date}%' order by b.time DESC;`
+  )
+    .then((result) => {
+      res.status(200).json(result[0]);
+    })
+    .catch((err) => console.log(err));
+};
 
 exports.createBooking = async (req, res, next) => {
-  const fieldId = req.body.fieldId
-  const playerId = req.body.playerId
-  const date = req.body.date
-  const status = req.body.status
-  const bookedTimestamp = '2023-02-21 11:00:02'
-  const pitchNumber = req.body.pitchNumber
-  const time = req.body.time
+  const fieldId = req.body.fieldId;
+  const playerId = req.body.playerId;
+  const date = req.body.date;
+  const status = req.body.status;
+  const bookedTimestamp = "2023-02-21 11:00:02";
+  const pitchNumber = req.body.pitchNumber;
+  const time = req.body.time; 
 
   db.execute(
     `INSERT INTO booking (field_id, player_id, date, status, booked_timestamp, pitch_number, time) VALUES ('${fieldId}',${playerId},'${date}','${status}','${bookedTimestamp}','${pitchNumber}','${time}')`
   )
     .then(
       res.status(201).json({
-        message: 'Field Booked Successfully',
+        message: "Field Booked Successfully",
       })
     )
-    .catch((err) => console.log(err))
-}
-
-exports.createBooking = async (req, res, next) => {
-  const fieldId = req.body.fieldId
-  const playerId = req.body.playerId
-  const date = req.body.date
-  const status = req.body.status
-  const bookedTimestamp = req.body.bookedTimestamp
-  const pitchNumber = req.body.pitchNumber
-  const time = req.body.time
-
-  db.execute(
-    `INSERT INTO booking (field_id, player_id, date, status, booked_timestamp, pitch_number, time) VALUES ('${fieldId}',${playerId},'${date}','${status}','${bookedTimestamp}','${pitchNumber}','${time}')`
-  )
-    .then(
-      res.status(201).json({
-        message: 'Field Booked Successfully',
-      })
-    )
-    .catch((err) => console.log(err))
-}  
+    .catch((err) => console.log(err));
+};
--- routes/booking.router.js
+++ routes/booking.router.js
@@ -10,6 +10,9 @@ router.get('/', bookingController.getAllBookings)
 // GET requset
 router.get('/mine/:playerId', bookingController.getMyBookings)
 
+// GET owner requset
+router.get('/owner', bookingController.getOwnerBooking)
+
 // POST req
 router.post('/', bookingController.createBooking)
 
